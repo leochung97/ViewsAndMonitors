5f087041a931f5534d339cc9b50d3546e2b831a0
Leo Chung <leo.chung97@gmail.com>
2023-12-05 16:28:40 -0500

    Add notes and additional ViewModifier styling

--- ViewsAndMonitors/ContentView.swift
+++ ViewsAndMonitors/ContentView.swift
@@ -7,15 +7,75 @@
 
 import SwiftUI
 
+// To create a custom modifier, create a new struct that conforms to the ViewModifier protocol
+// This has one requirement, which is a method called body that accepts whatever content it's being given to work with, and must return some View
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(.rect(cornerRadius: 10))
+    }
+}
+
+// When working with custom modifiers, it's a smart idea to create extensions on View that make them easier to use
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundStyle(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+struct CapsuleText: View {
+    var text: String
+
+    var body: some View {
+        Text(text)
+            .titleStyle()
+//          .modifier(Title())
+//          .font(.largeTitle)
+//          .padding()
+//          .foregroundStyle(.white)
+//          .background(.blue)
+//          .clipShape(.capsule)
+        
+        
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 10) {
+            CapsuleText(text: "First")
+            CapsuleText(text: "Second")
+            
         }
-        .padding()
+        
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Hacking with Swift")
     }
 }
 
