a6dd66103c42258775df84b413fa1f882e43ad7c
Leo Chung <leo.chung97@gmail.com>
2023-12-05 12:13:19 -0500

    Initial Commit

---
+++ ViewsAndMonitors/ViewsAndMonitorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndMonitorsApp.swift
+//  ViewsAndMonitors
+//
+//  Created by Leo Chung on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndMonitorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
