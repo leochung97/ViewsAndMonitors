aff5306f9240826f2c64ecb8e0b244ce3b0545c2
Leo Chung <leo.chung97@gmail.com>
2023-12-05 16:47:30 -0500

    Finish Project 3

--- ViewsAndMonitors/ContentView.swift
+++ ViewsAndMonitors/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  ViewsAndMonitors
-//
-//  Created by Leo Chung on 12/5/23.
-//
-
 import SwiftUI
 
 // To create a custom modifier, create a new struct that conforms to the ViewModifier protocol
@@ -48,6 +41,28 @@ extension View {
     }
 }
 
+// It's possible to create custom containers as well
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+//  let content: (Int, Int) -> Content
+    @ViewBuilder let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                    .padding(1)
+                }
+            }
+            .padding(1)
+        }
+    }
+}
+
 struct CapsuleText: View {
     var text: String
 
@@ -60,22 +75,25 @@ struct CapsuleText: View {
 //          .foregroundStyle(.white)
 //          .background(.blue)
 //          .clipShape(.capsule)
-        
-        
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 10) {
-            CapsuleText(text: "First")
-            CapsuleText(text: "Second")
-            
-        }
+//      VStack(spacing: 10) {
+//          CapsuleText(text: "First")
+//          CapsuleText(text: "Second")
+//
+//      }
+//
+//      Color.blue
+//          .frame(width: 300, height: 200)
+//          .watermarked(with: "Hacking with Swift")
         
-        Color.blue
-            .frame(width: 300, height: 200)
-            .watermarked(with: "Hacking with Swift")
+        GridStack(rows: 4, columns: 4) { row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+//          Text("R\(row) C\(col)")
+        }
     }
 }
 
